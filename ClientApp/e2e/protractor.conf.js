215f5c5a321b7a3501a2cc93199ffb22494e41c9
AZenheimer <assenheimer@plansysteme.de>
2020-05-05 12:10:46 +0200

    Pushed a lot of testing stuff
    - protractor.conf.js
    - minimal E2E-Test
    - package.json
    - Sarah Michelle Gellar as a new Item of the main page

--- ClientApp/e2e/protractor.conf.js
+++ ClientApp/e2e/protractor.conf.js
@@ -2,27 +2,54 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const { JUnitXmlReporter } = require('jasmine-reporters');
+
+process.env.CHROME_BIN = process.env.CHROME_BIN || require("puppeteer").executablePath();
 
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
+  multiCapabilities: [
+    /*
+    {
+      'browserName': 'chrome',
+      chromeOptions: {
+        args: ["disable-gpu", "--lang=de-DE", "--enable-browser-side-navigation"],
+        //args: ["--headless", "disable-gpu", "--lang=de-DE", "--enable-browser-side-navigation"],
+        binary: process.env.CHROME_BIN
+      }
+    },
+    */
+    {
+      'browserName': 'chrome',
+      chromeOptions: {
+        args: ["--headless", "disable-gpu", "--lang=en-US"],
+        //args: ["disable-gpu", "--lang=en-US"],
+        binary: process.env.CHROME_BIN
+      }
+    }
+  ],
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  //baseUrl: 'http://localhost:4200/',
+  //baseUrl: 'http://localhost:1103/',
+  baseUrl: 'https://halfpipe.azurewebsites.net',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () { }
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    var junitReporter = new JUnitXmlReporter({
+      savePath: require('path').join(__dirname, './junit'),
+      consolidateAll: true
+    });
+    jasmine.getEnv().addReporter(junitReporter);
   }
 };
--- ClientApp/e2e/src/app.e2e-spec.ts
+++ ClientApp/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,46 @@
 import { AppPage } from './app.po';
+import { element } from 'protractor';
+import { browser, by } from 'protractor';
 
 describe('App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
+  //---------------------------------------------------------------------------
+
+  beforeAll(() => {
     page = new AppPage();
+  });
+
+  beforeEach(() => {
+    browser.waitForAngularEnabled(false);
+  });
+
+  afterEach(() => {
+    browser.waitForAngularEnabled(true);
+  });
+
+  //---------------------------------------------------------------------------
+
+  beforeEach(() => {
+    browser.waitForAngularEnabled(false);
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getMainHeading()).toEqual('Hello, world!');
+    expect(page.getMainHeading()).toEqual('HalfPipe U');
+  });
+
+  it('check if all skaters are present', () => {
+    element.all(by.tagName('body')).getText().then((text) => {
+      expect(text[0]).toContain('Rodney Mullen');
+      expect(text[0]).toContain('Tony Hawk');
+    });
+  });
+
+  xit('check if all actresses are present', () => {
+    element.all(by.tagName('body')).getText().then((text) => {
+      expect(text[0]).toContain('Sarah Michelle Gellar');
+      expect(text[0]).toContain('Brie Larsson');
+    });
   });
 });
