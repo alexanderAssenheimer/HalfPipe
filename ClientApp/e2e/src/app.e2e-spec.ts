a2c84fb4bd7d35a67f4d8adc96a048488ec49345
AZenheimer <assenheimer@plansysteme.de>
2020-05-25 12:27:36 +0200

    Slot 'staging' has 10% traffic now. Am I still receiving 409 on deploy?

--- ClientApp/e2e/src/app.e2e-spec.ts
+++ ClientApp/e2e/src/app.e2e-spec.ts
@@ -80,6 +80,13 @@ describe('HalfPipe - Common E2E', () => {
     element(by.buttonText('Invite')).click();
   });
 
+  it('check version (should be the newest)', () => {
+    element.all(by.tagName('body')).getText().then((text) => {
+      expect(text[0]).toContain('33 should succeed');
+      //expect(text[0]).toContain('23 + 1');
+    });
+  });
+
   xit('should fail', () => {
     expect(true).toBeFalsy();
   });
